4006d1cc82a50caedc4a7e904f27aa68c80d7bcd
TeZzo1 <willaschek.t@gmail.com>
2016-11-26 17:00:37 +0100

    kontrola pow

--- proj2.c
+++ proj2.c
@@ -18,9 +18,7 @@ double taylorcf_pow(double x, double y, unsigned int n);
 
 int is_number(char * x);
 int parametr_n(char *x);
-
-double mylog(double x);
-double mypow(double x, double y);
+int kontrola_pow(double x, double y);
 
 void chyba();
 
@@ -28,25 +26,34 @@ int main(int argc, char **argv) {
     double x = 0;
     unsigned int n = 0;
     if(argc == 4 && !strcmp(argv[1], "--log")){
-        if(is_number(argv[2]) && parametr_n(argv[3])) {
+        //if(is_number(argv[2]) && parametr_n(argv[3])) {
+        if(parametr_n(argv[3])){
             x = strtod(argv[2], NULL);
             n = strtoul(argv[3], NULL, 10);
-            printf("       log(%g) = %.12g\n", x, log(x));
-            printf(" cfrac_log(%g) = %.12g\n", x, cfrac_log(x, n));
-            printf("taylor_log(%g) = %.12g\n", x, taylor_log(x, n));
+            if(n == 0)
+                chyba();
+            else{
+                printf("       log(%g) = %.12g\n", x, log(x));
+                printf(" cfrac_log(%g) = %.12g\n", x, cfrac_log(x, n));
+                printf("taylor_log(%g) = %.12g\n", x, taylor_log(x, n));
+            }
         }
         else
             chyba();
     }
     else if (argc == 5 && !strcmp(argv[1], "--pow")){
-        if(is_number(argv[2]) && is_number(argv[3]) && parametr_n(argv[4])){
+        //if(is_number(argv[2]) && is_number(argv[3]) && parametr_n(argv[4])){
+        if(parametr_n(argv[4])){
             n = strtoul(argv[4], NULL, 10);
             x = strtod(argv[2], NULL);
             double y = strtod(argv[3], NULL);
-            printf("         pow(%g,%g) = %.12g\n", x, y, pow(x, y));
-            printf("  taylor_pow(%g,%g) = %.12g\n", x, y, taylor_pow(x, y, n));
-            printf("taylorcf_pow(%g,%g) = %.12g\n", x, y, taylorcf_pow(x, y, n));
-
+            if(n == 0)
+                chyba();
+            else{
+                printf("         pow(%g,%g) = %.12g\n", x, y, pow(x, y));
+                printf("  taylor_pow(%g,%g) = %.12g\n", x, y, taylor_pow(x, y, n));
+                printf("taylorcf_pow(%g,%g) = %.12g\n", x, y, taylorcf_pow(x, y, n));
+            }
         }
         else{
             chyba();
@@ -68,8 +75,14 @@ int main(int argc, char **argv) {
 double taylor_log(double x, unsigned int n){
     if(x == 0)
         return -INFINITY;
-    if(x < 0)
+    if(x == INFINITY)
+        return INFINITY;
+    if(x == -INFINITY || x < 0)
         return NAN;
+    /*if(x == 0)
+        return -INFINITY;
+    if(x < 0)
+        return NAN;*/
     unsigned int i;
     double pom = 0;
     double vysledek = 0, pow_x = 1;
@@ -100,8 +113,11 @@ double taylor_log(double x, unsigned int n){
 double cfrac_log(double x, unsigned int n){
     if(x == 0)
         return -INFINITY;
-    if(x < 0)
+    if(x == INFINITY)
+        return INFINITY;
+    if(x == -INFINITY || x < 0)
         return NAN;
+
     double vysledek = 0;
     x = (x - 1) / (x + 1);
     unsigned int cislo = 2 * n + 1;
@@ -121,6 +137,24 @@ double cfrac_log(double x, unsigned int n){
  * @return - vysledek x^y
  */
 double taylor_pow(double x, double y, unsigned int n){
+    if((x == INFINITY || x == -INFINITY) && y > 0)
+        return INFINITY;
+    if(x != 0 && y == INFINITY)
+        return INFINITY;
+    if(y == INFINITY || y == -INFINITY || y == NAN || y == -NAN || x == INFINITY || x == -INFINITY ||
+       x == NAN || x == -NAN)
+        return kontrola_pow(x,y);
+    if(x < 0)
+        return NAN;
+    if(x == 1)
+        return 1;
+    if(x == 0 && y <= 0)
+        return INFINITY;
+    if(x == 0 && y > 0)
+        return 0;
+    if(x != 0 && y == 0)
+        return 1;
+
     double vysledek = 0, pom = 1;
     x = taylor_log(x, n);
     for(unsigned int i = 1; i <= n; i++){
@@ -139,6 +173,25 @@ double taylor_pow(double x, double y, unsigned int n){
  * @return - vysledek x^y
  */
 double taylorcf_pow(double x, double y, unsigned int n){
+    if((x == INFINITY || x == -INFINITY) && y > 0)
+        return INFINITY;
+    if(x != 0 && y == INFINITY)
+        return INFINITY;
+    if(y == INFINITY || y == -INFINITY || y == NAN || y == -NAN || x == INFINITY || x == -INFINITY ||
+        x == NAN || x == -NAN)
+        return kontrola_pow(x,y);
+    if(x < 0)
+        return NAN;
+    if(x == 1)
+        return 1;
+    if(x == 0 && y <= 0)
+        return INFINITY;
+    if(x == 0 && y > 0)
+        return 0;
+    if(x != 0 && y == 0)
+        return 1;
+
+
     double vysledek = 0, pom = 1;
     x = cfrac_log(x, n);
     for(unsigned int i = 1; i <= n; i++){
@@ -147,34 +200,79 @@ double taylorcf_pow(double x, double y, unsigned int n){
     }
     return 1 + vysledek;
 }
-/*
-
-
-double mylog(double x){
-
-}
-
-double mypow(double x, double y){
-    double vysledek = 0;
-    
-}*/
-
 
 
 void chyba(){
     printf("Spatne zadane parametry.\n");
 }
 
+int parametr_n(char *x){
+    for(unsigned int i = 0; i < strlen(x); i++)
+        if(x[i] < '0' || x[i] > '9')
+            return 0;
+    return 1;
+}
+
+int kontrola_pow(double x, double y){
+    if(y == NAN && x == 1)
+        return 1;
+    if(x == 0 && y == INFINITY)
+        return 0;
+    if(x == NAN || y == NAN || x == -NAN || y == -NAN)
+        return NAN;
+    if(x == NAN || x == -NAN)
+        return NAN;
+    if(x == INFINITY && y < 0)
+        return 0;
+    if(x == INFINITY && y > 0)
+        return INFINITY;
+
+    return 0;
+}
+
+
+/*
 int is_number(char *x){
+    (void)x;
+
+
     if(!strcmp(x, "nan") || !strcmp(x, "-nan") || !strcmp(x, "inf") || !strcmp(x, "-inf"))
         return 1;
-    if(x[0] != '-' || !(x[0] <= '9' || x[0] >= '0'))                        //x[0] != 'e' || x[0] != '+')
-        return 0;
-    int tecka = 0;
-    for(unsigned int i = 1; i < strlen(x); i++){
+    int tecka = 0, e = 0;
+    unsigned int i = 1;
+    if(x[0] != '-' && x[0] != '.')
+        if(x[0] > '9' || x[0] < '0'){
+            if(x[0] == 'e' && (x[1] == '+' || x[1] == '-')){
+                if(strlen(x) > 2){
+                    e++;
+                    i++;
+                }
+                else
+                    return 0;
+            }
+            else
+                return 0;
+        }
+
+    if(x[0] == '.')
+        tecka++;
+
+    for(; i < strlen(x); i++){
+        if(x[i] == '.')
+            tecka++;
+        else if(x[i] == 'e' && (x[i+1] == '+' || x[i+1] == '-'))
+            e++;
+        else if(x[i] < '0' || x[i] > '9')
+            return 0;
+        else if(e != 0 && x[i] == '.')
+            return 0;
 
     }
-    /*
+    if(tecka <= 1 || e <= 1)
+        return 1;
+    return 0;
+    //return 1;
+    *
     if(!strcmp(x, "nan") || !strcmp(x, "-nan") || !strcmp(x, "inf") || !strcmp(x, "-inf"))
         return 1;
     int tecka = 0;
@@ -192,12 +290,6 @@ int is_number(char *x){
         return 1;
     printf("Here\n");
     return 0;
-     */
+     *
 }
-
-int parametr_n(char *x){
-    for(unsigned int i = 0; i < strlen(x); i++)
-        if(x[i] < '0' || x[i] > '9')
-            return 0;
-    return 1;
-}
+*/
